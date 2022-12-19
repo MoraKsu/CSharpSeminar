49ced92fdbe211202853e81f4b81d3e32fd01081
Kseniya Erina <moraksu@gmail.com>
2022-12-19 12:50:32 +0400

    first commit

---
+++ Seminar1/Program.cs
@@ -0,0 +1,54 @@
+﻿// Семинар 1.
+
+// Задача 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+
+if(num1 == quad2)
+{
+  Console.WriteLine("Yes!");
+}
+else
+{
+  Console.WriteLine("No!");
+}
+*/
+
+// Задача 2. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+/*
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = -number;
+
+while(current <= number)
+{
+  Console.Write(current + " ");
+  current++; // current += 1, current = current + 1;
+}
+*/
+
+/*
+37145 / 10 = 3714
+37145 / 100 = 371
+37145 / 1000 = 37
+37145 / 10000 = 3
+37145 / 100000 = 0
+
+37145 % 10 = 5
+37145 % 100 = 45
+37145 % 1000 = 145
+37145 % 10000 = 7145
+37145 % 100000 = 37145
+
+56473 -> 64
+/100 %100
+
+14359 -> 35
+/10 %100
+*/
---
+++ Seminar2/Program.cs
@@ -0,0 +1,82 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+/*
+int CutNumber(int number)
+{
+  int sot = number / 100;
+  int ed = number % 10;
+
+  int result = sot * 10 + ed;
+  return result;
+}
+
+int randNumber = new Random().Next(100, 1000);
+Console.WriteLine("Current random three-digit number is " + randNumber);
+
+int newNumber = CutNumber(randNumber);
+Console.WriteLine("New version of number is " + newNumber);
+*/
+
+// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+/*
+int MaxNumber(int number)
+{
+  int des = number / 10;
+  int ed = number % 10;
+  int max = 0;
+
+  if (des > ed)
+  {
+    max = des;
+  }
+  else
+  {
+    max = ed;
+  }
+  return max;
+}
+
+int randNumber = new Random().Next(10, 100);
+Console.WriteLine("Текущее число " + randNumber);
+
+int newNumber = MaxNumber(randNumber);
+Console.WriteLine("Максимальное число из текущего " + newNumber);
+*/
+// Напишите программу, которая будет принимать на вход два числа и определять, является ли второе число кратным первому.
+
+/*
+bool InfoNumber(int num1, int num2)
+{
+  if (num2 % num1 == 0) return true;
+  else return false;
+} 
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(InfoNumber(num1, num2));
+*/
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+bool QuadroNumber(int num1, int num2)
+{
+  if (num1 == num2 * num2 || num2 == num1 * num1) return true;
+  else return false;
+}
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (QuadroNumber(num1, num2))
+{
+  Console.WriteLine("Одно из чисел является квадратом другого");
+}
+else
+{
+  Console.WriteLine("Ни одно число не является квадратом друг друга");
+}
